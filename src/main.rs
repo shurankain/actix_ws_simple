7be333fb7e2a94c68ed992adfe69a187660e7ed2
Oleksandr Husiev <ohusiev@icloud.com>
2024-10-08 22:52:29 +0200

    added POST endpoint logic to main.rs to calculate the greatest common divisor

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
 use actix_web::{web, App, HttpResponse, HttpServer};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct GcdParameters {
+    n: u64,
+    m: u64,
+}
 
 #[actix_web::main]
 async fn main() {
     let server = HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(get_index))
+            .route("/gcd", web::post().to(post_gcd))
     });
 
     println!("Serving on http://localhost:3000...");
@@ -28,4 +36,39 @@ async fn get_index() -> HttpResponse {
                 </form>
             "#,
         )
+}
+
+async fn post_gcd(form: web::Form<GcdParameters>) -> HttpResponse {
+    if form.m == 0 || form.n == 0 {
+        return HttpResponse::BadRequest()
+            .content_type("text/html")
+            .body("Computing GCD with zeroes is boring");
+    }
+    let response = format!("the greatest common divisor of the numbers {} and {} is <b>{}</b>\n",
+                           form.m, form.n, gcd(form.n, form.m));
+
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(response)
+}
+
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+        m = m % n;
+    }
+    n
+}
+
+#[test]
+fn test_gcd() {
+    assert_eq!(gcd(14, 15), 1);
+    assert_eq!(gcd(2 * 3 * 5 * 11 * 17,
+                   3 * 7 * 11 * 13 * 19),
+               3 * 11);
 }
